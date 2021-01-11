47b3ced594427a1993973247f965a5ea48f51e57
pierre <pierrecrochet03@gmail.com>
2021-01-11 15:56:02 +0100

    function reminder

--- section-1/play.js
+++ section-1/play.js
@@ -4,9 +4,34 @@ const hasHobbies = true;
 
 // name ='Maximilien'
 // Will output an error
-age 30;
+age = 30;
 
-function summarizeUser(userName, userAge, userHasHobby){
+// function summarizeUser(userName, userAge, userHasHobby){
+// 	return (
+// 		'Name is ' +
+// 		userName +
+// 		', age is ' +
+// 		userAge +
+// 		' and the user has hobbies: ' +
+// 		userHasHobby
+// 	);
+// }
+
+
+// const summarizeUser =function (userName, userAge, userHasHobby){
+// 	return (
+// 		'Name is ' +
+// 		userName +
+// 		', age is ' +
+// 		userAge +
+// 		' and the user has hobbies: ' +
+// 		userHasHobby
+// 	);
+// }
+
+
+const summarizeUser = (userName, userAge, userHasHobby) => {
+	// allow to use the 'this' statement
 	return (
 		'Name is ' +
 		userName +
@@ -17,4 +42,21 @@ function summarizeUser(userName, userAge, userHasHobby){
 	);
 }
 
+
+// const add = (a,b) => {
+// 	return a + b 
+// }
+
+// shorten
+const add = (a,b) =>  a + b;
+
+
+// one argument
+const addOne = a =>  a + 1;
+
+const addRandom = () => 1 + 2;
+
 console.log(summarizeUser(name, age, hasHobbies));
+console.log(add(1,2))
+console.log(addOne(1))
+console.log(addRandom());
