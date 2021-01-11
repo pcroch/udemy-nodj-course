2b059c67fbcdc37b5b63216951d9779e1f325675
pierre <pierrecrochet03@gmail.com>
2021-01-11 16:16:51 +0100

    has and this statements

--- section-1/play.js
+++ section-1/play.js
@@ -1,62 +1,23 @@
-const name = 'Max';
-let age = 29;
-const hasHobbies = true;
+// 'this' refers to thesurround object, here 'person'
 
-// name ='Maximilien'
-// Will output an error
-age = 30;
-
-// function summarizeUser(userName, userAge, userHasHobby){
-// 	return (
-// 		'Name is ' +
-// 		userName +
-// 		', age is ' +
-// 		userAge +
-// 		' and the user has hobbies: ' +
-// 		userHasHobby
-// 	);
+// const person = {
+// 	name: 'Max',
+// 	age: 29,
+// 	great: () => {
+// 		console.log('Hi, I am ' + this.name);
+// 	}
 // }
+// person.great()
+// will output
 
 
-// const summarizeUser =function (userName, userAge, userHasHobby){
-// 	return (
-// 		'Name is ' +
-// 		userName +
-// 		', age is ' +
-// 		userAge +
-// 		' and the user has hobbies: ' +
-// 		userHasHobby
-// 	);
-// }
 
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-	// allow to use the 'this' statement
-	return (
-		'Name is ' +
-		userName +
-		', age is ' +
-		userAge +
-		' and the user has hobbies: ' +
-		userHasHobby
-	);
+const person = {
+	name: 'Max',
+	age: 29,
+	great() {
+		console.log('Hi, I am ' + this.name);
+	}
 }
 
-
-// const add = (a,b) => {
-// 	return a + b 
-// }
-
-// shorten
-const add = (a,b) =>  a + b;
-
-
-// one argument
-const addOne = a =>  a + 1;
-
-const addRandom = () => 1 + 2;
-
-console.log(summarizeUser(name, age, hasHobbies));
-console.log(add(1,2))
-console.log(addOne(1))
-console.log(addRandom());
+person.great()
